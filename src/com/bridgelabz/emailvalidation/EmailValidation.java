494c5b6d49b7232f631e042683226308b96a8c86
shruthishruu05 <shruthi.m@perfios.com>
2021-09-07 20:19:47 +0530

    ADDED : method with email validation for mandatory username

--- src/com/bridgelabz/emailvalidation/EmailValidation.java
+++ src/com/bridgelabz/emailvalidation/EmailValidation.java
@@ -1,5 +1,22 @@
 package com.bridgelabz.emailvalidation;
-
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class EmailValidation {
+	public static boolean emailValidator(String email) {
+		Pattern pattern = Pattern.compile("([a-zA-Z][a-zA-Z0-9_-]*[a-z|A-Z|0-9])");
+		Matcher matcher = pattern.matcher(email);
+		return matcher.matches();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Enter an email id");
+		Scanner sc = new Scanner(System.in);
+		String email = sc.nextLine();
+		
+		if(emailValidator(email))System.out.println("valid email id");
+		else System.out.println("invalid email id");
 
+		sc.close();
+	}
 }
