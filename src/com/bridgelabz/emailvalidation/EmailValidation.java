41b74860c7318e6351a47dc62383baa328460c3c
shruthishruu05 <shruthi.m@perfios.com>
2021-09-07 20:33:08 +0530

    ADDED : method to validate the 2nd mandatory domain name

--- src/com/bridgelabz/emailvalidation/EmailValidation.java
+++ src/com/bridgelabz/emailvalidation/EmailValidation.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class EmailValidation {
 	public static boolean emailValidator(String email) {
-		Pattern pattern = Pattern.compile("([a-zA-Z][a-zA-Z0-9_-]*[a-z|A-Z|0-9])");
+		Pattern pattern = Pattern.compile("^[a-zA-Z]*[@][.a-zA-z]*$");
 		Matcher matcher = pattern.matcher(email);
 		return matcher.matches();
 	}
